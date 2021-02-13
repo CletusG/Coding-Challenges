3acd13e0e2d743cd39e3031ae48daf3ce170633a
cl3tu5 <53231555+CletusG@users.noreply.github.com>
2021-02-13 13:44:35 -0500

    Update count_bits.cpp

--- count_bits.cpp
+++ count_bits.cpp
@@ -1,3 +1,5 @@
+// The following works fine in Visual studio
+
 #include <iostream>
 
 using namespace std;
@@ -18,4 +20,15 @@ int main()
     count_bits(4); // put number here
 
     return 0;
+}
+
+// The following is what i used for CodeWars
+unsigned int countBits(unsigned long long n){
+  int ret = 0;
+  while (n)
+  {
+    ret += n & 1;
+    n >>= 1;
+  }
+  return ret;
 }
